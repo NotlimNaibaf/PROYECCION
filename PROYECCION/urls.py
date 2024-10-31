762e9f5b94511e5e812ec251f2c34985f888d993
NOTLIM <mfloresv2@unemi.edu.ec>
2024-10-30 23:00:20 -0500

    INICIO2

---
+++ PROYECCION/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from datos.views import CustomLoginView, CustomLogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('datos.urls')),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+]
---
+++ datos/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    RegistroUsuarioView, CustomLoginView, ListaDatoView, DetalleDatoView, CustomLogoutView)
+
+urlpatterns = [
+    path('', ListaDatoView.as_view(), name='pesos_granja'),
+
+    path('registro/', RegistroUsuarioView.as_view(), name='registro'),
+
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('Datos/<int:pk>/', DetalleDatoView.as_view(), name='graficos'),
+
+
+   ]
